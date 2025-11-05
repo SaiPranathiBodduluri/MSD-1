aeaea93149ca27f4c2620780e6502bb4f85bf0ad
SaiPranathiBodduluri <saipranathibodduluri@gmail.com>
2025-11-05 14:32:06 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://zw-backend-8jos.onrender.com/api/auth/login", {
+      const res = await axios.post("https://msd-1-1.onrender.com/api/auth/login", {
         email,
         password,
       });
